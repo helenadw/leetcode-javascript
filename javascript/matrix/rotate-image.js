727d84cc9062ad2d65dbde41144142c7657a2fe4
helenadw <25150955+helenadw@users.noreply.github.com>
2023-02-11 17:21:13 +0000

    rotate image

---
+++ javascript/matrix/rotate-image.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function(matrix) {
+    let n = matrix.length;
+    
+    for (let i = 0; i < n; i++) {
+        for (let j = i; j < n; j++) {
+            let temp = matrix[i][j]
+            matrix[i][j] = matrix[j][i]
+            matrix[j][i] = temp
+        }
+    }
+    
+    for (let i = 0; i < n; i++ ) {
+        let start = 0;
+        let end = n-1;
+        while (start < end) {
+            let temp = matrix[i][start];
+            matrix[i][start] = matrix[i][end];
+            matrix[i][end] = temp;
+            start++;
+            end--;
+        }
+    }
+};
