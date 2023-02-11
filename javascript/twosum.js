ee46ce1d02c12515aa6146116991b2a7515a4c07
helenadw <25150955+helenadw@users.noreply.github.com>
2023-02-11 15:48:21 +0000

    first commit

---
+++ javascript/twosum.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let map = {};
+    for (let i = 0; i < nums.length; i++) {
+        let value = nums[i];
+        let pair = target - value;
+        if (map[pair] !== undefined) {
+            return [map[pair], i]
+        } else {
+            map[value] = i;
+        }
+    }
+};
+
+// sort the array find first value equal to target and ignore rest
