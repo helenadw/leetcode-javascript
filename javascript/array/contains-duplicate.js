efb817512a40aa77d13b59038cf3725973b8cd83
helenadw <25150955+helenadw@users.noreply.github.com>
2023-02-11 17:23:44 +0000

    contains duplicate

---
+++ javascript/array/contains-duplicate.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    let map = {};
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] in map) {
+            return true
+        }
+        map[nums[i]] = 1;
+    }
+    return false;
+};
